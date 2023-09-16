21345ac60b91e79502eac377c3133180cc4640ff
Bishnu Dev <devbishnu2000@gmail.com>
2023-09-16 16:55:37 +0400

    added comments

--- Assignment2/client.c
+++ Assignment2/client.c
@@ -9,32 +9,39 @@
 #define SERVER_PORT 9000
 
 int main() {
+    // Create a UDP socket for the client
     int clientFD = socket(AF_INET, SOCK_DGRAM, 0);
     if (clientFD < 0) {
         fprintf(stderr, "Error creating socket\n");
         exit(1);
     }
 
-    // Reuse address
+    // Enable reusing the same address and port for multiple runs
     if (setsockopt(clientFD, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
         fprintf(stderr, "Error setting socket options for reusing address\n");
         exit(1);
     }
 
+    // Define the server's address and port
     struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(SERVER_PORT);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    // Prepare the request message. Any message will do
     char requestString[] = "Time request";
 
+    // Send the request message to the server, blocking call
     sendto(clientFD, requestString, sizeof(requestString), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+
+    // Receive the server's response and save it in a buffer. Also a blocking call
     char responseString[1024];
     recvfrom(clientFD, responseString, sizeof(responseString), 0, 0, 0);
 
+    // Display the server's response (assumes it contains the time)
     printf("Server time: %s\n", responseString);
 
-    // Close connection
+    // Close the socket and release resources
     close(clientFD);
 
     return 0;
--- Assignment2/server.c
+++ Assignment2/server.c
@@ -6,27 +6,40 @@
 #include<string.h>
 #include<netinet/in.h>
 #include <time.h>
-
+#include <signal.h>
 
 #define PORT 9000
 
+int serverFD;
+
+void exitHandler(int signum) {
+    // Close the socket and release resources.
+    close(serverFD);
+    printf("Time server stopped\n");
+    exit(0);
+}
+
 int main() {
-    int serverFD = socket(AF_INET, SOCK_DGRAM, 0);
+    // Create a UDP socket for the server.
+    serverFD = socket(AF_INET, SOCK_DGRAM, 0);
     if (serverFD < 0) {
         fprintf(stderr, "Error creating socket\n");
         exit(1);
     }
 
+    // Enable reusing the same address and port for multiple runs.
     if (setsockopt(serverFD, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
         fprintf(stderr, "Error setting socket options for reusing address\n");
         exit(1);
     }
 
+    // Define the server's address and port.
     struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
+    // Bind the socket to the specified address and port.
     if (bind(serverFD, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         fprintf(stderr, "Error binding socket\n");
         exit(1);
@@ -34,26 +47,47 @@ int main() {
         printf("Time server started:\n");
     }
 
+    // Setup signal handler to close the socket when Ctrl+C is pressed.
+    if (signal(SIGINT, exitHandler) == SIG_ERR) {
+        perror("Error setting up signal handler to hanlde Ctrl+C\n");
+        exit(1);
+    }
+
+    // Set up variables for handling client requests.
     struct sockaddr_in clientAddr;
     socklen_t clientAddrLen = sizeof(clientAddr);
+
+    // Enter a loop to continuously handle incoming requests.
     while (1) {
         char buffer[1024];
-        recvfrom(serverFD, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
 
-        // Convert IP address from binary to text form
+        // Receive the request from a client.
+        int msgLength = recvfrom(serverFD, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
+        if (msgLength < 0) {
+            fprintf(stderr, "Error receiving message\n");
+            exit(1);
+        }
+
+        // Convert the client's IP address from binary to text form.
         char clientIP[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
+        if (inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN) == NULL) {
+            fprintf(stderr, "Error converting IP address to string\n");
+            exit(1);
+        }
 
+        // Print information about the incoming request.
         printf("Time request from: %s:%d\n", clientIP, ntohs(clientAddr.sin_port));
 
+        // Get the current time and format it as a string.
         time_t rawTime = time(NULL);
         char* rawTimeStr = ctime(&rawTime);
 
-        sendto(serverFD, rawTimeStr, strlen(rawTimeStr), 0, (struct sockaddr *)&clientAddr, clientAddrLen);
+        // Send the current time back to the client.
+        if (sendto(serverFD, rawTimeStr, strlen(rawTimeStr), 0, (struct sockaddr *)&clientAddr, clientAddrLen) < 0) {
+            fprintf(stderr, "Error sending message\n");
+            exit(1);
+        }
     }
-
-    // Close connection
-    close(serverFD);
 
     return 0;
 }
